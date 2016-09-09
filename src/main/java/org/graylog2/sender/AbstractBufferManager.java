dc041fbdb9e415b5df3a0055391c7d04c1550d8e
Sami Korhonen <skorhone@gmail.com>
2016-09-09 15:01:31 +0300

    Refactor: Optimized gzipMessage (removed unnecessary copying between buffers)

--- src/main/java/org/graylog2/sender/AbstractBufferManager.java
+++ src/main/java/org/graylog2/sender/AbstractBufferManager.java
@@ -2,26 +2,18 @@ package org.graylog2.sender;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.zip.GZIPOutputStream;
 
 public abstract class AbstractBufferManager {
 	protected byte[] gzipMessage(String message) {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		try {
-			GZIPOutputStream stream = new GZIPOutputStream(bos);
-			byte[] bytes;
-			try {
-				bytes = message.getBytes("UTF-8");
-			} catch (UnsupportedEncodingException e) {
-				throw new RuntimeException("No UTF-8 support available.", e);
-			}
-			stream.write(bytes);
-			stream.finish();
-			stream.close();
-			byte[] zipped = bos.toByteArray();
-			bos.close();
-			return zipped;
+			Writer writer = new OutputStreamWriter(new GZIPOutputStream(bos), "utf-8");
+			writer.write(message);
+			writer.close();
+			return bos.toByteArray();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
