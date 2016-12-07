4bf04e75e1f70a9d4ce6db3915562f0abb0a069b
Sami Korhonen <skorhone@gmail.com>
2016-12-07 19:37:42 +0200

    + Refactor: Simplified log4j GelfAppender

--- src/main/java/org/graylog2/log/GelfAppender.java
+++ src/main/java/org/graylog2/log/GelfAppender.java
@@ -1,11 +1,16 @@
 package org.graylog2.log;
 
+import java.lang.reflect.Method;
 import java.util.logging.ErrorManager;
 
 import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.MDC;
 import org.apache.log4j.spi.ErrorCode;
+import org.apache.log4j.spi.LocationInfo;
 import org.apache.log4j.spi.LoggingEvent;
 import org.graylog2.message.GelfMessage;
+import org.graylog2.message.GelfMessageBuilder;
 import org.graylog2.message.GelfMessageBuilderConfiguration;
 import org.graylog2.message.GelfMessageBuilderException;
 import org.graylog2.sender.GelfSender;
@@ -13,25 +18,33 @@ import org.graylog2.sender.GelfSenderConfiguration;
 import org.graylog2.sender.GelfSenderConfigurationException;
 import org.graylog2.sender.GelfSenderException;
 import org.graylog2.sender.GelfSenderFactory;
+import org.graylog2.util.Fields;
 
 /**
  *
  * @author Anton Yakimov
  * @author Jochen Schalanda
  */
-public class GelfAppender extends AppenderSkeleton implements GelfMessageProvider {
+public class GelfAppender extends AppenderSkeleton {
+	private static Method methodGetTimeStamp = null;
+	private static final String LOGGER_NDC = "loggerNdc";
 	private GelfMessageBuilderConfiguration gelfMessageBuilderConfiguration;
 	private GelfSenderConfiguration senderConfiguration;
 	private GelfSender gelfSender;
-	private GelfMessageFactory messageFactory;
 	private boolean extractStacktrace;
 	private boolean addExtendedInformation;
 	private boolean includeLocation;
 
+	static {
+		try {
+			methodGetTimeStamp = LoggingEvent.class.getDeclaredMethod("getTimeStamp");
+		} catch (Exception ignoredException) {
+		}
+	}
+
 	public GelfAppender() {
 		this.gelfMessageBuilderConfiguration = new GelfMessageBuilderConfiguration();
 		this.senderConfiguration = new GelfSenderConfiguration();
-		this.messageFactory = new GelfMessageFactory();
 		this.includeLocation = true;
 	}
 
@@ -151,7 +164,7 @@ public class GelfAppender extends AppenderSkeleton implements GelfMessageProvide
 			errorHandler.error("Could not send GELF message. Gelf Sender is not initialised and equals null");
 		} else {
 			try {
-				GelfMessage gelfMessage = messageFactory.makeMessage(layout, event, this);
+				GelfMessage gelfMessage = createMessage(event);
 				sender.sendMessage(gelfMessage);
 			} catch (GelfMessageBuilderException exception) {
 				errorHandler.error("Error building GELF message", exception, ErrorCode.WRITE_FAILURE);
@@ -166,18 +179,67 @@ public class GelfAppender extends AppenderSkeleton implements GelfMessageProvide
 		return gelfSender;
 	}
 
-	public GelfMessageFactory getMessageFactory() {
-		return messageFactory;
-	}
-
 	public void close() {
-		GelfSender x = this.getGelfSender();
-		if (x != null) {
-			x.close();
+		if (gelfSender != null) {
+			gelfSender.close();
 		}
 	}
 
 	public boolean requiresLayout() {
 		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	private GelfMessage createMessage(LoggingEvent event) throws GelfMessageBuilderException {
+		long timeStamp = getTimeStamp(event);
+		Level level = event.getLevel();
+
+		GelfMessageBuilder builder = new GelfMessageBuilder(getGelfMessageBuilderConfiguration());
+
+		if (isIncludeLocation()) {
+			LocationInfo locationInformation = event.getLocationInformation();
+			builder.addField(GelfMessageBuilder.CLASS_NAME_FIELD, locationInformation.getClassName());
+			builder.addField(GelfMessageBuilder.METHOD_NAME_FIELD, locationInformation.getMethodName());
+		}
+
+		builder.setLevel(String.valueOf(level.getSyslogEquivalent()));
+		builder.setTimestamp(timeStamp);
+		builder.setMessage(formatMessage(event));
+		if (event.getThrowableInformation() != null) {
+			builder.setThrowable(event.getThrowableInformation().getThrowable());
+		}
+
+		if (isAddExtendedInformation()) {
+			builder.addField(GelfMessageBuilder.THREAD_NAME_FIELD, event.getThreadName());
+			builder.addField(GelfMessageBuilder.NATIVE_LEVEL_FIELD, level.toString());
+			builder.addField(GelfMessageBuilder.LOGGER_NAME_FIELD, event.getLoggerName());
+			builder.addFields(MDC.getContext());
+			String ndc = event.getNDC();
+			if (ndc != null) {
+				builder.addField(LOGGER_NDC, event.getNDC());
+			}
+		}
+		builder.addFields(Fields.getFields(event.getMessage()));
+
+		return builder.build();
+	}
+
+	private String formatMessage(LoggingEvent event) {
+		String renderedMessage = layout != null ? layout.format(event) : event.getRenderedMessage();
+		if (renderedMessage == null) {
+			renderedMessage = "";
+		}
+		return renderedMessage;
+	}
+
+	private long getTimeStamp(LoggingEvent event) {
+		long timeStamp = 0;
+		if (methodGetTimeStamp != null) {
+			try {
+				timeStamp = (Long) methodGetTimeStamp.invoke(event);
+			} catch (Exception ignoredException) {
+			}
+		}
+		return timeStamp == 0 ? System.currentTimeMillis() : timeStamp;
 	}
 }
